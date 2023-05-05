ec0d57f218c1603af20c73b81a678f14d662ab73
Vinícius Patrício <vpms@icomp.ufam.edu.br>
2023-05-05 12:24:41 -0400

    webcam acess and optical flow

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from open_cv.optical_flow import *
 if __name__ == '__main__':
 
     video_path = "videos/VID_20230407_154458.mp4"
-    method = " "
-    #optical_flow(method, video_path)
+    method1 = cv2.optflow.calcOpticalFlowSparseToDense
 
-    webcam_acess()
+    # optical_flow(method, video_path)
+    # webcam_acess()
+
+    # params = [8, 128, 0.001, True, 500.0, 3]
+    # webcam_optical_flow(method1, params)
+
+    method2 = cv2.calcOpticalFlowFarneback
+    params2 = [0.3, 5, 10, 5, 11, 1.5, 0]
+    webcam_optical_flow(method2, params2)
--- open_cv/optical_flow.py
+++ open_cv/optical_flow.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 
 def optical_flow(method, video_path):
@@ -21,15 +22,47 @@ def optical_flow(method, video_path):
 
     print("Tempo total de execução: ", end_time-start_time, "segundos.")
 
+
 def webcam_acess():
     cap = cv2.VideoCapture(0)
 
     while True:
-    
-        ret, frame = cap.read()
-        
-        cv2.imshow('Camera', frame)
 
+        ret, frame = cap.read()
+        filter_frame = cv2.medianBlur(frame, 3)
+        cv2.imshow('Camera', frame)
+        cv2.imshow('Imagem filtrada', filter_frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def webcam_optical_flow(method, params=[]):
+
+    cap = cv2.VideoCapture(0)
+
+    ret, old_frame = cap.read()
+    hsv = np.zeros_like(old_frame)
+    hsv[..., 1] = 255
+    prevs_frame = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+
+    while True:
+        cv2.imshow('Camera', old_frame)
+
+        ret, new_frame = cap.read()
+        next_frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
+
+        flow = method(prevs_frame, next_frame, None, *params)
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang*180/np.pi/2
+        hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+        cv2.imshow('New_frame', bgr)
+        old_frame = new_frame
+        prevs_frame = next_frame
 
         if cv2.waitKey(1) == ord('q'):
             break
